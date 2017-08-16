e4c9cbaa0b383f796fd96026119e3d77fbed2e04
Tyler Malone <rtylermalone@gmail.com>
2017-08-16 10:48:15 -0400

    upgrade to stand alone prop-types package

--- src/Reveal.js
+++ src/Reveal.js
@@ -6,7 +6,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 const
   propTypes = {
